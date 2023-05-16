1946a91e4d594e9381c8d85e8183c6e6bd2b1913
Steven McArdle <steve@well.as>
2023-05-16 13:01:07 +1200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,82 @@
+import { history } from 'prosemirror-history';
+import { Plugin } from 'prosemirror-state';
+import { dropCursor } from 'prosemirror-dropcursor';
+import { gapCursor } from 'prosemirror-gapcursor';
+import { menuBar } from 'prosemirror-menu';
+
+import Placeholder from './Placeholder';
+
+export { EditorState, Selection } from 'prosemirror-state';
+export { EditorView } from 'prosemirror-view';
+import {
+  listInputRules,
+  linksInputRules,
+  blocksInputRule,
+  baseKeyMaps,
+  textFormattingInputRules,
+} from './rules';
+
+import { buildArticleEditorMenu } from './menu/article';
+import { buildMessageEditorMenu } from './menu/message';
+
+export { MessageMarkdownTransformer } from './schema/markdown/messageParser';
+export { ArticleMarkdownTransformer } from './schema/markdown/articleParser';
+
+export { ArticleMarkdownSerializer } from './schema/markdown/articleSerializer';
+export { MessageMarkdownSerializer } from './schema/markdown/messageSerializer';
+
+export { fullSchema } from './schema/article';
+export { messageSchema } from './schema/message';
+
+export function plaitArticleWriterSetup(props) {
+  let plugins = [
+    history(),
+    baseKeyMaps(props.schema),
+    blocksInputRule(props.schema),
+    textFormattingInputRules(props.schema),
+    linksInputRules(props.schema),
+    listInputRules(props.schema),
+    dropCursor(),
+    gapCursor(),
+    Placeholder(props.placeholder),
+    menuBar({
+      floating: true,
+      content: buildArticleEditorMenu(props.schema, props.onImageUpload)
+        .fullMenu,
+    }),
+    new Plugin({
+      props: {
+        attributes: { class: 'ProseMirror-plait-style' },
+      },
+    }),
+    ...(props.plugins || []),
+  ];
+
+  return plugins;
+}
+
+export function plaitMessageWriterSetup(props) {
+  let plugins = [
+    ...(props.plugins || []),
+    history(),
+    baseKeyMaps(props.schema),
+    blocksInputRule(props.schema),
+    textFormattingInputRules(props.schema),
+    linksInputRules(props.schema),
+    listInputRules(props.schema),
+    dropCursor(),
+    gapCursor(),
+    Placeholder(props.placeholder),
+    menuBar({
+      floating: true,
+      content: buildMessageEditorMenu(props.schema).fullMenu,
+    }),
+    new Plugin({
+      props: {
+        attributes: { class: 'ProseMirror-plait-style' },
+      },
+    }),
+  ];
+
+  return plugins;
+}
